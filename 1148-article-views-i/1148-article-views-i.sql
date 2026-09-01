ed4235684e4c6ad79028200c8802213dc49abd3f
Hansika Vangari <2303a52409@sru.edu.in>
2026-09-01 17:15:36 +0530

    Time: 1236 ms (5%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,2 +1,3 @@
-SELECT DISTINCT author_id AS id
-FROM Views WHERE author_id = viewer_id ORDER BY id;
+# Write your MySQL query statement below
+select distinct author_id as id
+from views where author_id = viewer_id order by id;
